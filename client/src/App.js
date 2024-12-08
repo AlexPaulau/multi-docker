683ae601eef5feb1282b7ca69d6c37491a2d0ebb
AlexPaulau <aliaksandr.paulau@gmail.com>
2024-12-08 17:50:56 +0100

    feat: try provided files

--- client/src/App.js
+++ client/src/App.js
@@ -1,39 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
-import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
-import OtherPage from './OtherPage';
-import Fib from './Fib';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import OtherPage from "./OtherPage";
+import Fib from "./Fib";
 
 function App() {
   return (
-
-
-    <div className="App">
-      <Router>
-        <header>
+    <Router>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
           <Link to="/">Home</Link>
           <Link to="/otherpage">Other Page</Link>
         </header>
         <Routes>
-          <Route path="/fib" element={<Fib />} />
+          <Route path="/" element={<Fib />} />
           <Route path="/otherpage" element={<OtherPage />} />
         </Routes>
-      </Router>
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      </div>
+    </Router>
   );
 }
 
